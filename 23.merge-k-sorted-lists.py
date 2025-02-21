c0d628f87d6fb9a9c9fd0e3b57d68a0d7a613173
Varun Verma <varun.verma2024@gmail.com>
2025-02-21 17:43:09 +0530

    optimal merge K heads of LL done

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -10,9 +10,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        mergedHead = ListNode(-1)
+        minheap = []
+        temp = mergedHead
         
+        for head in lists:
+            if head:
+                heapq.heappush(minheap, (head.val, id(head), head))
+        
+        while minheap:
+            val,_,node = heapq.heappop(minheap)
+            if node.next:
+                heapq.heappush(minheap, (node.next.val, id(node.next), node.next))
+            temp.next = node
+            temp = temp.next
+        
+        return mergedHead.next
+            
         
 # @lc code=end
 
