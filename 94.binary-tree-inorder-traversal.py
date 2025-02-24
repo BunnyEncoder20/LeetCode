cfca7316ee1986a548923cd3af0ca447c6158d8b
Varun Verma <varun.verma2024@gmail.com>
2025-02-24 21:21:12 +0530

    morris inorder done

--- 94.binary-tree-inorder-traversal.py
+++ 94.binary-tree-inorder-traversal.py
@@ -14,14 +14,35 @@
 from typing import Optional
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        def recursive(node):
-            if not node: return
-            recursive(node.left)
-            res.append(node.val)
-            recursive(node.right)
-        recursive(root)
-        return res
+        # morris traversal TC:O(n) | SC:O(1) using threaded BT
+        inorder = []
+        if not root: return inorder
+        temp = root
+        
+        while temp:
+            if not temp.left:
+                inorder.append(temp.val)
+                temp = temp.right
+            else:
+                # find rightmost node in LST
+                rightMostNode = temp.left
+                while rightMostNode.right and rightMostNode.right != temp:
+                    rightMostNode = rightMostNode.right
+                
+                if rightMostNode.right == None:
+                    rightMostNode.right = temp
+                    temp = temp.left
+                else:
+                    rightMostNode.right = None
+                    inorder.append(temp.val)
+                    temp = temp.right
+        
+        return inorder
+                        
+                
+                    
+                
+                
         
 # @lc code=end
 
