d1104604eb3e20dadbf536b43ea9590903fe2f69
Varun Verma <varun.verma2024@gmail.com>
2025-02-26 16:40:07 +0530

    maximum-width-BT done

--- 662.maximum-width-of-binary-tree.py
+++ 662.maximum-width-of-binary-tree.py
@@ -14,16 +14,29 @@
 from typing import Optional
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        def getHeight(node):
-            if not node:
-                return 0
-            lsth = getHeight(node.left)
-            rsth = getHeight(node.right)
-            maxWidth[0] = max(maxWidth[0], 1 + lsth + rsth)
-            return 1 + max(lsth, rsth)
-        maxWidth = [0]
-        getHeight(root)
-        return maxWidth[0]-1
-        
+        if not root:
+            return root
+        maxWidth = -math.inf
+        q = deque([(root, 0)])
+        while q:
+            n = len(q)
+            levelMin = q[0][1]
+            first,last = 0,0
+            for i in range(n):
+                node, idx = q.popleft()
+                idx -= levelMin
+                if i == 0:
+                    first = idx
+                if i == n-1:
+                    last = idx
+                    
+                if node.left:
+                    q.append((node.left, 2*idx+1))
+                if node.right:
+                    q.append((node.right, 2*idx+2))
+            #end 
+            maxWidth = max(maxWidth, last-first+1)
+        # end
+        return maxWidth
 # @lc code=end
 
