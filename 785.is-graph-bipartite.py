b09a23c4c6818d0e8c59968a1d929a72092577f2
Varun Verma <varun.verma2024@gmail.com>
2025-03-03 10:12:50 +0530

    bipartite-graph-check done

--- 785.is-graph-bipartite.py
+++ 785.is-graph-bipartite.py
@@ -7,23 +7,20 @@
 # @lc code=start
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        def bfs(root):
-            q = deque([root])
-            colored[root] = False
-            while q:
-                node = q.popleft()
-                for it in graph[node]:
-                    if not colored[it]:
-                        colored[it] = not colored[node]
-                        q.append(it)
-                    elif colored[it] == colored[node]:
+        def dfs(node, node_color):
+            colored[node] = node_color
+            for it in graph[node]:
+                if not colored[it]:
+                    if not dfs(it, not node_color):
                         return False
+                elif colored[it] == node_color:
+                    return False 
             return True
         
         colored = [None]*len(graph)
         for i in range(len(graph)):
             if not colored[i]:
-                completed = bfs(i)
+                completed = dfs(i, False)
                 if not completed: return False
         return True
                 
