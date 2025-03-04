9e039f71a82805a8e3c44dcb5ac8d21bc26d66d6
Varun Verma <varun.verma2024@gmail.com>
2025-03-04 07:53:05 +0530

    median-of-2-sorted-arr done again

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -7,36 +7,32 @@
 # @lc code=start
 from typing import List
 class Solution:
+    from typing import List
+
+class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1,n2 = len(nums1),len(nums2)
+        n1, n2 = len(nums1), len(nums2)
         if n1 > n2: 
             nums1,nums2 = nums2,nums1
             n1,n2 = n2,n1
-        if n1 == 0:
-            if n2%2==1: return nums2[n2//2]
-            return (nums2[n2//2] + nums2[n2//2 - 1]) / 2
         
-        on_your_left = (n1+n2+1)//2
+        needed_left = (n1+n2+1)//2
         low,high = 0,n1
-        
         while low<=high:
             mid1 = (low+high)//2
-            mid2 = on_your_left - mid1
-            
-            # mid elements
+            mid2 = needed_left - mid1
             r1 = nums1[mid1] if mid1 < n1 else float('inf')
             r2 = nums2[mid2] if mid2 < n2 else float('inf')
             l1 = nums1[mid1-1] if mid1-1 >= 0 else float('-inf')
             l2 = nums2[mid2-1] if mid2-1 >= 0 else float('-inf')
             
-            if (l1<=r2 and l2<=r1):
-                if (n1+n2)%2==1: return max(l1,l2)
-                return (max(l1,l2) + min(r1,r2)) / 2
+            if l1<=r2 and l2<=r1:
+                if (n1+n2)%2 == 1: return max(l1,l2)
+                else: return (max(l1,l2)+min(r1,r2)) / 2
             elif l1 > r2:
                 high = mid1-1
             else:
                 low = mid1+1
-        
         return -1
                 
         
