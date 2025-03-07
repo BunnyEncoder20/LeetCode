cdd11a24126ff1693a8c22b7c0ee6b4069c32ea1
Varun Verma <varun.verma2024@gmail.com>
2025-03-07 11:41:15 +0530

    fibonacci-tabulation done

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -8,7 +8,9 @@
 class Solution:
     def fib(self, n: int) -> int:
         # return self.recursion(n)
-        return self.memoization(n)
+        # return self.memoization(n)
+        # return self.tabulation(n)
+        return self.spaceOptimizedTabulation(n)
     
     def recursion(self, n):
         if n<=1: return n
@@ -25,6 +27,41 @@ class Solution:
         dp = [None] * (n+1)
         res = recursive(n)
         return res
+    
+    def tabulation(self, n):
+        # base case 
+        if n == 0: return 0
+        if n == 1: return 1
+        
+        # init
+        dp = [None] * (n+1)
+        dp[0],dp[1] = 0,1
+        
+        # remaining cases: use recursive relation
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
+    
+    def spaceOptimizedTabulation(self, n):
+        # base case 
+        if n == 0: return 0
+        if n == 1: return 1 
+        
+        # init 
+        prev2 = 0
+        prev1 = 1
+        curr = None
+        
+        # remaining cases
+        for i in range(2,n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+            
+        return curr
+        
         
 # @lc code=end
 
