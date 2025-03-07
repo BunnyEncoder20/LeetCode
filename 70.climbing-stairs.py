063313f2db1815772a498a44a79cad3a2dd4cdf7
Varun Verma <varun.verma2024@gmail.com>
2025-03-07 12:00:43 +0530

    climbing-stairs tabulation done

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -30,6 +30,22 @@ class Solution:
         dp[0],dp[1] = 1,1
         recursive(n)
         return dp[n]
+
+    def tabulation(self, n):
+        # base case
+        if n <= 1: return 1
+        
+        # init 
+        dp = [-1]*(n+1)
+        dp[0],dp[1] = 1,1
+        
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
+        
+        
     
         
 # @lc code=end
