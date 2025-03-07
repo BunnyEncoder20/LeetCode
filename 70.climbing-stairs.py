042bedcce9cc6d54fbc5efa2187636753d63213d
Varun Verma <varun.verma2024@gmail.com>
2025-03-07 11:56:43 +0530

    climbing-stairs memo done

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -7,11 +7,29 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return self.recursion(n)
+        # return self.recursion(n)
+        # return self.memoization(n)
+        return self.tabulation(n)
 
     def recursion(self, n):
         if n <= 1: return 1
         return self.recursion(n-1) + self.recursion(n-2)
+    
+    def memoization(self, n):
+        def recursive(n):
+            if n <= 1: return 1
+            if dp[n] != -1: return dp[n]
+            dp[n] = recursive(n-1) + recursive(n-2)
+            return dp[n]
+        
+        # base case 
+        if n <= 1: return 1
+        
+        # init
+        dp = [-1] * (n+1)
+        dp[0],dp[1] = 1,1
+        recursive(n)
+        return dp[n]
     
         
 # @lc code=end
