cc70ac608f419f04b67dee72a5bb3b75cc4939c1
Varun Verma <varun.verma2024@gmail.com>
2025-03-07 12:03:07 +0530

    climbing-stairs spaceOptimized

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -36,13 +36,14 @@ class Solution:
         if n <= 1: return 1
         
         # init 
-        dp = [-1]*(n+1)
-        dp[0],dp[1] = 1,1
+        curr,prev2,prev1 = None,1,1
         
         for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
         
-        return dp[n]
+        return curr
         
         
         
