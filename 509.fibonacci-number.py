e6571732858826ff900cae6437a370c9febbe9cb
Varun Verma <varun.verma2024@gmail.com>
2025-03-07 11:12:44 +0530

    fibonacci-memoization done

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -7,15 +7,24 @@
 # @lc code=start
 class Solution:
     def fib(self, n: int) -> int:
-        return self.recursion(n)
-        # self.memoization(n)
+        # return self.recursion(n)
+        return self.memoization(n)
     
     def recursion(self, n):
         if n<=1: return n
         return self.recursion(n-1)+self.recursion(n-2)
         
     def memoization(self, n):
-        pass
+        def recursive(n):
+            if n<=1: return n
+            if dp[n] != None: return dp[n]
+            dp[n] = recursive(n-1) + recursive(n-2)
+            return dp[n]
+
+        # init
+        dp = [None] * (n+1)
+        res = recursive(n)
+        return res
         
 # @lc code=end
 
