8f38ce41faed918a25a0204308f7e88a1a73fa0d
Varun Verma <varun.verma2024@gmail.com>
2025-03-11 06:17:10 +0530

    longest-common-subseq memo done

---
+++ 1143.longest-common-subsequence.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=1143 lang=python3
+#
+# [1143] Longest Common Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+      def recursive(i1, i2):
+        # base case
+        if i1 < 0 or i2 < 0:
+          return 0
+        
+        # dp check
+        if dp[i1][i2] != -1: return dp[i1][i2]
+        
+        # calc 
+        if text1[i1] == text2[i2]:
+          dp[i1][i2] = 1 + recursive(i1-1, i2-1)
+          return dp[i1][i2]
+        else:
+          dp[i1][i2] = max(recursive(i1 - 1,i2), recursive(i1,i2 - 1))
+          return dp[i1][i2]
+      
+      n,m = len(text1),len(text2)
+      dp = [[-1] * m for _ in range(n)]
+      return recursive(n-1, m-1)
+        
+# @lc code=end
+
