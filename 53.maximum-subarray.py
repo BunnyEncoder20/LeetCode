766b744eb50c4b691830674c47320010c5196938
Varun Verma <varun.verma2024@gmail.com>
2025-03-12 07:12:05 +0530

    just practise runs

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -7,17 +7,18 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxsum = float('-inf')
-        sum = 0
-        
-        for num in nums:
-          sum += num
-          if sum > maxsum:
-            maxsum = sum
-          if sum < 0:
-            sum = 0
-        
-        return maxsum
+      maxsum = float('-inf')
+      sum = 0
+      
+      for num in nums:
+        sum += num
+        if sum > maxsum:
+          maxsum = sum
+        if sum < 0:
+          sum = 0
+      
+      return maxsum
+            
           
 # @lc code=end
 
