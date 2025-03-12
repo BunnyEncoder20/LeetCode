b023c2f200a70554eee94cd08a68838d39dff950
Varun Verma <varun.verma2024@gmail.com>
2025-03-12 07:07:20 +0530

    maximum-subarray-sum done

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = float('-inf')
+        sum = 0
+        
+        for num in nums:
+          sum += num
+          if sum > maxsum:
+            maxsum = sum
+          if sum < 0:
+            sum = 0
+        
+        return maxsum
+          
+# @lc code=end
+
