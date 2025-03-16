310397398ef431bbc3d23c369411cb19b5eb7e44
Varun Verma <varun.verma2024@gmail.com>
2025-03-16 11:31:27 +0530

    unique-paths memo done

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -5,8 +5,24 @@
 #
 
 # @lc code=start
+from functools import lru_cache
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        
+        @lru_cache(None)
+        def recursive(i,j):
+            # base case 
+            if i == m-1 or j == n-1:
+                return 1 
+            
+            # normally
+            goRight = recursive(i, j+1)
+            goDown = recursive(i+1, j)
+            
+            return goRight + goDown
+
+        return recursive(0,0)
+        
         
 # @lc code=end
 
