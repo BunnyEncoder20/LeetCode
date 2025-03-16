e8638f723a9436958e969b2ee0f6207051ac0adc
Varun Verma <varun.verma2024@gmail.com>
2025-03-16 11:56:59 +0530

    uniquePaths added line for most optimal

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -22,6 +22,9 @@ class Solution:
             return goRight + goDown
 
         return recursive(0,0)
+       
+        # Combination Method (choosing down moves (m-1) from total ways (m-1 + n-1))
+        # return math.comb(m+n-2, m-1)
         
         
 # @lc code=end
