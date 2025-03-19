58e68f6435d0755651f0fb3b063da9cd80946cec
Varun Verma <varun.verma2024@gmail.com>
2025-03-19 14:28:52 +0530

    house-robber-memo done

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -11,7 +11,10 @@ class Solution:
             # base case
             if i == 0: return nums[0]
             if i < 0: return 0
-
+            
+            # dp check
+            if dp[i] != -1: return dp[i]
+            
             # normally
             # not rob
             notrob = 0 + recursive(i-1)
@@ -20,9 +23,11 @@ class Solution:
             rob = nums[i] + recursive(i-2)
 
             # return the best
-            return max(rob, notrob)
+            dp[i] = max(rob, notrob)
+            return dp[i]
         
         n = len(nums)
+        dp = [-1] * n
         return recursive(n-1)
         
         
