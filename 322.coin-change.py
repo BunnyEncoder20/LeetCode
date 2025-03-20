08c48804ff6ff0555c9931c0a1cd78b01501c3a4
Varun Verma <varun.verma2024@gmail.com>
2025-03-20 12:53:17 +0530

    coin-change-spaceOptim done

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -10,30 +10,32 @@ class Solution:
         def MinimumCoins(self, coins, amount):
             # init
             n = len(coins)
-            dp = [[0] * (amount+1) for _ in range(n)]
+            prev = [0] * (amount+1)
+            curr = [0] * (amount+1)
 
             # init oth row of table
             for T in range(amount+1):
-                if T % coins[0] == 0: dp[0][T] = T // coins[0]
-                else: dp[0][T] = float('inf')
+                if T % coins[0] == 0: prev[T] = T // coins[0]
+                else: prev[T] = float('inf')
 
             for i in range(1, n):
                 for T in range(amount+1):
                     # init
-                    take, nottake = float('inf'), float('inf') 
+                    take, nottake = float('inf'), None
 
                     # not take
-                    nottake = 0 + dp[i-1][T]
+                    nottake = 0 + prev[T]
                     
                     # take
                     if coins[i] <= T:
-                        take = 1 + dp[i][T - coins[i]]
+                        take = 1 + curr[T - coins[i]]
                     
-                    dp[i][T] = min(take, nottake)
+                    curr[T] = min(take, nottake)
             
-            if dp[n-1][amount] >= int(1e9): return -1
+            ans = prev[amount]
+            if ans >= int(1e9): return -1
             
-            return dp[n-1][amount]
+            return ans
         
 # @lc code=end
 
