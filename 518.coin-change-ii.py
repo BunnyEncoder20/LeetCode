6c950111119770b9a4164d2c570911a7a97d64a9
Varun Verma <varun.verma2024@gmail.com>
2025-03-20 14:29:03 +0530

    coin-change-II-tabulation done

--- 518.coin-change-ii.py
+++ 518.coin-change-ii.py
@@ -5,33 +5,26 @@
 #
 
 # @lc code=start
-from functools import lru_cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        
-        @lru_cache(None)
-        def recursive(i, need):
-            # base case 
-            if i == 0: 
-                if need % coins[0] == 0:
-                    return 1 
-                else:
-                    return 0
-            
-            # normally
-            pick, notpick = 0, 0
-            
-            # notpick 
-            notpick = recursive(i-1, need)
-            
-            # pick
-            if coins[i] <= need:
-                pick = recursive(i, need - coins[i])
-            
-            return pick + notpick
-
+        # init 
         n = len(coins)
-        return recursive(n-1, amount)
+        dp = [[0] * (amount+1) for _ in range(n)]
+        
+        # init 0th row of dp table
+        for T in range(amount+1):
+            if T % coins[0] == 0: dp[0][T] = 1
+        
+        for i in range(1, n):
+            for T in range(amount+1):
+                pick, notpick = 0, 0
+                notpick = dp[i-1][T]
+                if coins[i] <= T:
+                    pick = dp[i][T - coins[i]]
+                dp[i][T] = pick + notpick
+        
+        return dp[n-1][amount]
+    
             
         
 # @lc code=end
