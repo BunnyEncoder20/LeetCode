9f8d6d095bd4ff68122f76c68b08032e9eae3c2a
Varun Verma <varun.verma2024@gmail.com>
2025-03-20 19:27:22 +0530

    word-break-tabulation done

--- 139.word-break.py
+++ 139.word-break.py
@@ -7,21 +7,20 @@
 # @lc code=start
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def recursive(i):
-            # base case 
-            if i < 0: return True
-            
-            # check all words
-            for word in wordDict:
-                if s[i-len(word)+1 : i+1] == word and recursive(i - len(word)):
-                    return True
-            
-            # none matched
-            return False
-
         n = len(s)
-        return recursive(n-1)
+        dp = [False] * n
+        
+        for i in range(n):
+            for word in wordDict:
+                # i not of length of wordDict word
+                if i < len(word)-1: continue
+                
+                if i == len(word)-1 or dp[i-len(word)]:
+                    if s[i - len(word)+1 : i+1] == word:
+                        dp[i] = True
+                        break
+            
+        return dp[n-1]
         
 # @lc code=end
 
