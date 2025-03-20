7e23ee2dd299eff3465adf8bfe3af3dc6e8971af
Varun Verma <varun.verma2024@gmail.com>
2025-03-20 14:45:15 +0530

    coin-change-II-spaceOptim done

--- 518.coin-change-ii.py
+++ 518.coin-change-ii.py
@@ -9,21 +9,23 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         # init 
         n = len(coins)
-        dp = [[0] * (amount+1) for _ in range(n)]
+        prev = [0] * (amount+1)
+        curr = [0] * (amount+1)
         
         # init 0th row of dp table
         for T in range(amount+1):
-            if T % coins[0] == 0: dp[0][T] = 1
+            if T % coins[0] == 0: prev[T] = 1
         
         for i in range(1, n):
             for T in range(amount+1):
                 pick, notpick = 0, 0
-                notpick = dp[i-1][T]
+                notpick = prev[T]
                 if coins[i] <= T:
-                    pick = dp[i][T - coins[i]]
-                dp[i][T] = pick + notpick
+                    pick = curr[T - coins[i]]
+                curr[T] = pick + notpick
+            prev[:] = curr
         
-        return dp[n-1][amount]
+        return prev[amount]
     
             
         
