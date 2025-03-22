d976d1da04889076db164b6ad5adcb429e56a0d7
Varun Verma <varun.verma2024@gmail.com>
2025-03-22 12:14:47 +0530

    grid-unique-paths-spaceOptim done

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -8,20 +8,23 @@
 from functools import lru_cache
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        prev = [0] * n
         
-        @lru_cache(None)
-        def recursive(i,j):
-            # base case 
-            if i == m-1 or j == n-1:
-                return 1 
-            
-            # normally
-            goRight = recursive(i, j+1)
-            goDown = recursive(i+1, j)
-            
-            return goRight + goDown
+        for i in range(m):
+            curr = [0] * n
+            for j in range(n):
+                if i == 0 and j == 0: 
+                    curr[j] = 1
+                    continue
+                
+                up,left = 0,0
+                if i > 0: up = prev[j]
+                if j > 0: left = curr[j-1]
+                curr[j] = up + left
+            prev[:] = curr
+        
+        return prev[n-1]
 
-        return recursive(0,0)
        
         # Combination Method (choosing down moves (m-1) from total ways (m-1 + n-1))
         # return math.comb(m+n-2, m-1)
