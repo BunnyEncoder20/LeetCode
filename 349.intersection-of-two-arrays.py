8af7592aabe79cbb2b3576867e383c0ac94a1327
Varun Verma <varun.verma2024@gmail.com>
2025-03-22 09:07:49 +0530

    intersection-arr trolling solution done

--- 349.intersection-of-two-arrays.py
+++ 349.intersection-of-two-arrays.py
@@ -7,23 +7,7 @@
 # @lc code=start
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
-        n,m = len(nums1), len(nums2)
-        p1,p2 = 0,0
-        res = set()
-        
-        while p1 < n and p2 < m:
-            if nums1[p1] < nums2[p2]:
-                p1 += 1
-            elif nums1[p1] > nums2[p2]:
-                p2 += 1
-            else:
-                res.add(nums1[p1])
-                p1 += 1
-                p2 += 1
-        
-        return list(res)
+        return list(set(nums1) & set(nums2))
         
         
 # @lc code=end
