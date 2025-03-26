f7a44eb36e71a01ad124f20c655efcda8c333eb9
Varun Verma <varun.verma2024@gmail.com>
2025-03-26 21:45:39 +0530

    kth-smallest-element-sorted-mat-bsearch done

--- 378.kth-smallest-element-in-a-sorted-matrix.py
+++ 378.kth-smallest-element-in-a-sorted-matrix.py
@@ -5,28 +5,43 @@
 #
 
 # @lc code=start
-import heapq
+import bisect
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def upperBound(candidate, arr):
+            low, high = 0, len(arr)-1
+            while low<=high:
+                mid = (low+high)//2
+                if arr[mid] > candidate:
+                    high = mid-1
+                else:
+                    low = mid+1
+            return low
+        
+        def countLower(candidate):
+            count = 0
+            for row in matrix:
+                # count += upperBound(candidate, row)
+                count += bisect.bisect_right(row, candidate)
+            return count
+        
+        # init
         n = len(matrix)
-        heap = []
+        low = matrix[0][0]
+        high = matrix[n-1][n-1]
         
-        # Insert first col elements into heap
-        # pushing (val, row, col)
-        for i in range(n):
-            heapq.heappush(heap, (matrix[i][0], i, 0))
+        # BS the range
+        while low <= high:
+            mid = (low + high)//2
+            lowerCount = countLower(mid)
+            
+            # check
+            if lowerCount < k:
+                low = mid+1
+            else:
+                high = mid-1
         
-        # extract the smallest K elements
-        val = None
-        for _ in range(k):
-            val, r, c = heapq.heappop(heap)
-            if c+1 < n:
-                heapq.heappush(heap, (matrix[r][c+1], r, c+1))
-
-        return val
-                
-        
-        
+        return low
         
 # @lc code=end
 
