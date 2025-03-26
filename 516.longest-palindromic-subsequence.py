45d9ed3f47f1df3a05dc1e6210caa6e917ae2a2c
Varun Verma <varun.verma2024@gmail.com>
2025-03-26 20:34:34 +0530

    longest-plaindromic-subseq-tabulation-done

--- 516.longest-palindromic-subsequence.py
+++ 516.longest-palindromic-subsequence.py
@@ -5,30 +5,26 @@
 #
 
 # @lc code=start
-from functools import cache
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        @cache
-        def lcs(i, j):
-            # base case
-            if i<0 or j<0: 
-                return 0
-            
-            # matched
-            if s[i] == t[j]:
-                return 1 + lcs(i-1, j-1)
-            
-            # not matched
-            else:
-                return max(lcs(i-1, j), lcs(i, j-1))
-            
-        n = len(s)
-        t = s[::-1]
-        return lcs(n-1, n-1)
-        # return self.lcs(s, s[::-1])
+        return self.lcs(s, s[::-1])
     
     def lcs(self, s, t):
-        pass
+        n = len(s)
+        dp = [[0] * (n+1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(
+                        dp[i-1][j],
+                        dp[i][j-1]
+                    )
+
+        return dp[n][n]
+        
         
         
 # @lc code=end
