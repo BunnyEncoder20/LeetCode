f8da6b937bbcc447b0845c54c05431eb21461c30
Varun Verma <varun.verma2024@gmail.com>
2025-03-26 20:35:50 +0530

    longest-palindromic-subseq-spaceOptim done

--- 516.longest-palindromic-subsequence.py
+++ 516.longest-palindromic-subsequence.py
@@ -11,19 +11,21 @@ class Solution:
     
     def lcs(self, s, t):
         n = len(s)
-        dp = [[0] * (n+1) for _ in range(n+1)]
+        curr = [0] * (n+1)
+        prev = [0] * (n+1)
 
         for i in range(1, n+1):
             for j in range(1, n+1):
                 if s[i-1] == t[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    curr[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(
-                        dp[i-1][j],
-                        dp[i][j-1]
+                    curr[j] = max(
+                        prev[j],
+                        curr[j-1]
                     )
+            prev[:] = curr
 
-        return dp[n][n]
+        return prev[n]
         
         
         
