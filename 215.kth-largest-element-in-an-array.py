7c9872df1040cde071d5b620026af9bd107c5dcc
Varun Verma <varun.verma2024@gmail.com>
2025-03-29 13:08:03 +0530

    LL-cycle done

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -5,18 +5,41 @@
 #
 
 # @lc code=start
+from typing import List
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        minHeap = []
-        for n in nums:
-            if len(minHeap) == k:
-                heapq.heappushpop(minHeap, n)
-            else:
-                heapq.heappush(minHeap, n)
-        return minHeap[0]
+        # trivial case
+        n = len(nums)
+        if n == 0 or k > n: return -1
+        
+        heap = []
+        
+        # first k element pushed into heap
+        for num in nums[:k]:
+            heapq.heappush(heap, num)
+        
+        print(heap)
+        
+        # remaing elements
+        for num in nums[k:]:
+            heapq.heappushpop(heap, num)
+        
+        print(heap)
+        
+        # check for same nums 
+        if k == 1:
+            return heap[0] if len(heap) == 1 else -1
+        
+        return heap[0]
+        
+        
+        
+        
+               
                 
         
         
 # @lc code=end
+
 
