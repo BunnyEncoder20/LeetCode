921f2b6a37087325759faa19f7f864d5ba249d70
Varun Verma <varun.verma2024@gmail.com>
2025-04-08 19:52:50 +0530

    studying LLD

--- 5010.temp.py
+++ 5010.temp.py
@@ -1,32 +1,10 @@
-from abc import ABC, abstractmethod
+class Temp:
+    def __init__(self, name, position):
+        self.name = name 
+        self.position = position
+    
+    @staticmethod
+    def solution(x, y):
+        print(f"Name:{x} | Position:{y}")
 
-# Abstract interface for any payment method
-class PaymentProcessor(ABC):
-    @abstractmethod
-    def pay(self, amount):
-        pass
-
-# Stripe implements the interface
-class StripePayment(PaymentProcessor):
-    def pay(self, amount):
-        print(f"Paid ${amount} using Stripe")
-
-# PayPal implements the same interface
-class PayPalPayment(PaymentProcessor):
-    def pay(self, amount):
-        print(f"Paid ${amount} using PayPal")
-
-# Store depends on abstraction, not concrete classes
-class Store:
-    def __init__(self, payment_processor: PaymentProcessor):
-        self.payment_processor = payment_processor
-
-    def checkout(self, amount):
-        self.payment_processor.pay(amount)
-
-# Usage
-store1 = Store(StripePayment())
-store1.checkout(100)
-
-store2 = Store(PayPalPayment())
-store2.checkout(200)
+Temp.solution("Varun", "NA")
