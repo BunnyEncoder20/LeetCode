b3c726ce307b351ddbdfdb567d49fd824c33230a
Varun Verma <varun.verma2024@gmail.com>
2025-04-13 21:15:45 +0530

    completed Adapter design  code

--- 5016.Adapter_Design.py
+++ 5016.Adapter_Design.py
@@ -1,40 +1,45 @@
 from time import sleep
 
+# XML class (Legacy / Incompatible)
 class Xml():
     def __init__(self, xmldata: str):
-        # protected 
-        _xmldata = xmldata
-        
-    # public 
-    def getXmldata(self):
-        return _xmldata
+        self._xmldata = xmldata  # fixed attribute
 
+    def getXmldata(self):
+        return self._xmldata
+
+# Target Class (expects JSON)
 class DataAnalytics_Tool():
     def __init__(self, argJSONdata: str):
-        # protected
-        _jsondata = argJSONdata
-    
-    def analyseData():
+        self._jsondata = argJSONdata
+
+    def analyseData(self):
         print("⭕ Analysing data: ", self._jsondata)
         sleep(1)
         print("✅ Analysis completed.")
 
-class Cliet():
+# Adapter class to convert XML to JSON-like structure
+class XmlToJsonAdapter(DataAnalytics_Tool):
+    def __init__(self, xml: Xml):
+        # convert XML to fake JSON (simulating transformation)
+        converted = f'{{"converted_from_xml": "{xml.getXmldata()}"}}'
+        super().__init__(converted)
+
+# Client class
+class Client():
     def processData(self, tool: DataAnalytics_Tool):
         tool.analyseData()
 
 
-
-# client accessing the tool
+# Client side using the tool (it should not change the process of using our tool)
 if __name__ == "__main__":
-    # sample xml data
+    # sample XML data
     to_analyse_data_xml = Xml("!! Sample XML data !!")
 
-    # passing xml data (adapter to make this compartible with the tool)
-    analysis_tool = DataAnalytics_Tool(to_analyse_data_xml)
-    
-    # client analyzing the data
+    # Adapter used to convert and pass XML as JSON
+    # Instead of the tool itself, we make object of Adapter and pass the data the same
+    analysis_tool = XmlToJsonAdapter(to_analyse_data_xml)
+
+    # Client processes data with analytics tool
     client_xyz = Client()
     client_xyz.processData(analysis_tool)
-    
-    return
