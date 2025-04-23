3c969d37c0ad786c4be3e89e51f96c2fb5ab43a3
Varun Verma <varun.verma2024@gmail.com>
2025-04-23 12:32:02 +0530

    done with db appender file

---
+++ LLD Logger Framework/database_appender.py
@@ -0,0 +1,32 @@
+import psycopg2
+from log_appender import LogAppender
+
+class DatabaseAppender(LogAppender):
+    def __init__(self, db_url, username, password):
+        self.db_url = db_url
+        self.username = username
+        self.password = password
+    
+    def append(self, log_message):
+        try:
+            connection = psycopg2.connect(
+                dbname='your_db',
+                user='your_user',
+                password='your_password',
+                host='localhost',
+                port='5432'
+            )
+            # or using url
+            # connection = psycopg2.connect(self.db_url)
+
+            cursor = connection.cursor()
+            cursor.execute(
+                "INSERT INTO logs (level, message, timestamp) VALUES (%s, %s, %s)",
+                (log_message.get_level().name, log_message.get_message(), log_message.get_timestamp())
+            )
+            connection.commit()
+            cursor.close()
+            connection.close()
+        
+        except psycopg2.Error as e:
+            print(f"Error: {e}")
