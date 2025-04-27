5ddbd19e0f8bb918ec4ab1ae4d64e16133c7f7c3
Varun Verma <varun.verma2024@gmail.com>
2025-04-27 15:03:45 +0530

    completed task_manager file

--- LLD Task Management System/task_manager.py
+++ LLD Task Management System/task_manager.py
@@ -19,4 +19,65 @@ class TaskManager:
             cls()
         return cls._instance
 
+    def create_task(self, task):
+        self.tasks[task.get_id()] = task
+        self._assign_task_to_user(task.get_assigned_user(), task)
+        
+    def update_task(self, updated_task):
+        existing_task = self.tasks.get(updated_task.get_title())
+        if existing_task:
+            exisiting_task.set_title(updated_task.get_title())
+            existing_task.set_desc(updated_task.get_desc())
+            existing_task.set_due_date(updated_task.get_due_date())
+            existing_task.set_priority(updated_task.get_priority())
+            existing_task.set_status(updated_task.get_status())
+            
+            # check if we need to updated assigned user
+            previously_assigned_user = existing_task.get_assigned_user()
+            new_assigned_user = updated_task.get_assigned_user()
+            if previously_assigned_user != new_assigned_user:
+                self._unassign_task_from_user(previously_assigned_user, existing_task)
+                self._assign_task_to_user(new_assigned_user, existing_task)
+        else:
+            raise Exception("⚠️ specified task could not be found")
     
+    def delete_task(self, task_id):
+        task = self.tasks.pop(task_id, None)
+        if task: 
+            self._unassign_task_from_user(task.get_assigned_user(), task)
+    
+    def search_tasks(self, keyword):
+        matching_tasks = []
+        for task in self.tasks.values():
+            if keyword in task.get_title() or keyword in task.get_desc():
+                matching_tasks.append(task)
+        return matching_tasks
+    
+    def filter_tasks(self, status, start_date, end_date, priority):
+        filtered_tasks = []
+        for task in self.tasks.values():
+            if (
+                task.get_status() == status and
+                start_date <= task.get_due_date() <= end_date and
+                task.get_priority() == priority
+            ): filtered_tasks.append(task)
+        
+        return filtered_tasks
+    
+    def mark_task_as_completed(self, task_id):
+        task = self.tasks.get(task_id)
+        if task: task.set_status(TaskStatus.COMPLETED)
+        else:
+            raise Exception("⚠️ Specified task does not exisit")
+        return 
+    
+    def get_task_history(self, user):
+        return self.user_tasks.get(user.get_id(), [])
+    
+    def _assign_task_to_user(self, user):
+        self.user_tasks.setdefault(user.get_id(), []).append(task)
+        
+    def _unassign_task_from_user(self, user, task):
+        tasks = self.iser_tasks.get(user.get_id())
+        if tasks:
+            tasks.remove(task)
