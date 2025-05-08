f0f99e0385257bff94b8e7861bcc892f4cac79c4
Varun Verma <varun.verma2024@gmail.com>
2025-05-08 19:48:44 +0530

    updated rotten oranges code

--- 994.rotting-oranges.py
+++ 994.rotting-oranges.py
@@ -8,52 +8,44 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-      n,m = len(grid), len(grid[0])
-      directions = [(0,1), (1,0), (-1,0), (0,-1)]
-      fresh = 0
-      time = 0
-
-      # DS req
-      q = deque()
-
-      # traverse and collect all rotten oranges
-      for i in range(n):
-          for j in range(m):
-              # count fresh oranges
-              if grid[i][j] == 1:
-                  fresh += 1
-
-              # if rotten, push into rotten queue
-              if grid[i][j] == 2:
-                  q.append((0,i,j))
-      
-      # chk of valid cell
-      def isValid(i,j):
-          return 0 <= i < n and 0 <= j < m and grid[i][j] == 1
-
-      # BFS loop
-      while(q):
-          t, row, col = q.popleft()
-          time = max(time, t)
-          
-          # checkout all neighbours
-          for dr, dc in directions:
-              nrow, ncol = row+dr, col+dc
-
-              # if fresh orange found
-              if isValid(nrow, ncol):
-                  grid[nrow][ncol] = 2    # mark
-                  fresh -= 1              # reduce
-                  q.append((t+1, nrow, ncol))
-      
-      # return time if no more fresh oranges left 
-      return -1 if fresh>0 else time
-      
-      # fresh_count = 0, all oranges converted
-      return time if fresh_count == 0 else -1 
-
+        m,n = len(grid), len(grid[0])
+        time = 0
+        freshOranges = 0
+        q = deque()
         
+        # count fresh oranges &
+        # collect the rotten oranges into q
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    freshOranges += 1
+                elif grid[i][j] == 2:
+                   q.append((0, i, j))
         
+        # func to check for adj fresh orange
+        def isValid(i,j):
+            return (
+                0 <= i < m and 
+                0 <= j < n and 
+                grid[i][j] == 1
+            )
+
+        # BFS
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        while q:
+            t, i, j = q.popleft()
+            time = max(t, time)
+            
+            for di, dj in directions:
+                newi, newj = i+di, j+dj
+                if isValid(newi, newj):
+                    grid[newi][newj] = 2
+                    freshOranges -= 1
+                    q.append((t+1, newi, newj))
         
+        if freshOranges == 0:
+            return time
+        else:
+            return -1
 # @lc code=end
 
