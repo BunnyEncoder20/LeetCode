43b8bfec940ae1b17c4ecef5a46d0d2ceb13036d
Varun Verma <varun.verma2024@gmail.com>
2025-05-08 11:17:48 +0530

    celebrity problem optimal olution completed

--- 5016.celebrity-problem.py
+++ 5016.celebrity-problem.py
@@ -1,17 +1,46 @@
 class Solution:
     def celebrity(self, M):
         n = len(M)
-        knowMe = [0]*n
-        Iknow = [0]*n
-        for i in range(n):
-            for j in range(n):
-                if M[i][j]:
-                    knowMe[j] += 1
-                    Iknow[i] += 1
+        top = 0
+        down = n-1
         
-        # check for celebrity
-        for i in range(n):
-            if knowMe[i] == n-1 and Iknow[i] == 0:
-                return i
+        while top < down:
+            # if top knows down, top cannot be celebrity
+            if M[top][down]:
+                top += 1
+
+            # if down knows top, down cannot be celebrity
+            elif M[down][top]:
+                down -= 1
+            
+            # if both don't know eachother,
+            # both cannot be celebrity
+            else:
+                top += 1
+                down -= 1
         
-        return -1
+        # didn't find celebrity 
+        if top > down: 
+            return -1
+
+        # else top == down
+        # verify
+        for i in range(n):
+            if i == top: continue
+
+            # if top knows i or i doesn't know top,
+            # top is not a celeb
+            if M[top][i] or not M[i][top]:
+                return -1
+
+        # verified celeb
+        return top
+
+if __name__ == "__main__":
+    M = [
+        [0, 1, 1, 0], 
+        [0, 0, 0, 0], 
+        [1, 1, 0, 0], 
+        [0, 1, 1, 0]
+    ]
+    print(Solution().celebrity(M))
