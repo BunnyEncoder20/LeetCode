56e432ac8441736126ce5c3e2198ebf09edb4e36
Varun Verma <varun.verma2024@gmail.com>
2025-05-12 18:28:01 +0530

    rev letter - digit logs

--- 937.reorder-data-in-log-files.py
+++ 937.reorder-data-in-log-files.py
@@ -10,15 +10,14 @@ class Solution:
         letter_logs = []
         digit_logs = []
         
-        # seperate the logs
         for log in logs:
             if log[-1].isdigit():
                 digit_logs.append(log)
             else:
                 letter_logs.append(log)
         
-        # sort the letter logs
-        letter_logs.sort(key=lambda log: (" ".join(log.split()[1:]), log.split()[0]))
+        # sorting letter logs lexicographically
+        letter_logs.sort(key= lambda log: (" ".join(log.split()[1:]),log.split()[0])) 
         
         return letter_logs + digit_logs
 # @lc code=end
