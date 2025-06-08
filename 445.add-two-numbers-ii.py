a09c033a533fa7cb0e1c7618597a9f82a408feed
Varun Verma <varun.verma2024@gmail.com>
2025-06-08 13:00:51 +0530

    rev add-two-nums-LL-II

--- 445.add-two-numbers-ii.py
+++ 445.add-two-numbers-ii.py
@@ -12,50 +12,47 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        len1, len2 = self.getLength(l1), self.getLength(l2)
-        if len1 < len2:
-            l1 = self.addPadding(l1, len2-len1)
-        elif len1 > len2:
-            l2 = self.addPadding(l2, len1-len2)
+        len1, len2 = self.getlen(l1), self.getlen(l2)
+
+        # pad if the lengths are unequal
+        if len1 > len2: l2 = self.getpad(l2, len1-len2)
+        elif len2 > len1: l1 = self.getpad(l1, len2-len1)
         
-        carry, result = self.addLists(l1,l2)
+        # recursively add from lsb
+        result = ListNode(-1)
+        carry, result = self.getSum(l1, l2)
         if carry:
-            new = ListNode(carry)
-            new.next = result
-            result = new
-        
+            newhead = ListNode(carry, result)
+            result = newhead
         return result
     
-    def getLength(self, temp):
+    def getlen(self, temp):
         length = 0
+        if not temp: return length
+        
         while temp:
             length += 1
             temp = temp.next
-        return length
-
-    def addPadding(self, head, padding):
-        for _ in range(padding):
-            new = ListNode(0)
-            new.next = head
-            head = new
-        return head
         
-    def addLists(self, l1, l2):
+        return length
+             
+    def getpad(self, ll, n):
+        for _ in range(n):
+            padnode = ListNode(0, ll)
+            ll = padnode
+        return ll
+    
+    def getSum(self, l1, l2):
         if not l1 and not l2:
             return (0, None)
-
-        carry, nextNode = self.addLists(l1.next, l2.next)
+        
+        carry, prevhead = self.getSum(l1.next, l2.next)
         
         total = l1.val + l2.val + carry
-        carry,total = divmod(total, 10)
-        newNode = ListNode(total)
-        newNode.next = nextNode
-        return (carry, newNode)
-            
-             
+        carry, total = divmod(total, 10)
+        newhead = ListNode(total, prevhead)
         
-            
-        
+        return (carry, newhead)
         
 # @lc code=end
 
