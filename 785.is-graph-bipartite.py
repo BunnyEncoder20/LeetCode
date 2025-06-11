aa93ff4caa2068d879ecd86d832ba4338d01e4c7
Varun Verma <varun.verma2024@gmail.com>
2025-06-11 10:29:14 +0530

    completed biparte revision

--- 785.is-graph-bipartite.py
+++ 785.is-graph-bipartite.py
@@ -5,26 +5,58 @@
 #
 
 # @lc code=start
+from collections import deque
 class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        def dfs(node, node_color):
-            colored[node] = node_color
-            for it in graph[node]:
+    def isBipartite(self, adj: List[List[int]]) -> bool:
+        def dfs(node, prevNodeColor):
+            colored[node] = -1*prevNodeColor    # mark opp color
+            for it in adj[node]:
                 if not colored[it]:
-                    if not dfs(it, not node_color):
-                        return False
-                elif colored[it] == node_color:
-                    return False 
-            return True
-        
-        colored = [None]*len(graph)
-        for i in range(len(graph)):
-            if not colored[i]:
-                completed = dfs(i, False)
-                if not completed: return False
-        return True
+                    flag = dfs(it, colored[node])
+                    
+                    # if false from futher in traversal, return it
+                    if not flag: return False
                 
+                # the node was already colored
+                elif colored[it] == colored[node]:
+                    return False
+            
+            return True
+            
+
+        def bfs(snode,scolor):
+            q = deque([(snode, scolor)])
+
+            while q:
+                node, color = q.popleft()
+                for it in adj[node]:
+                    # If not colored
+                    if not colored[it]:
+                        colored[it] = -1*color      # color opposite of parent node color
+                        q.append((it, -1*color))    # enque adj node
+
+                    # If colored same color as parent node
+                    elif colored[it] == color:
+                        return False
+            
+            # traverse the graph, didn't flag false
+            return True
+
+        V = len(adj)
+        colored = [0]*V  # will color 1 and -1
+
+        for node in range(V):
+            if not colored[node]:
+                completely_biparted = dfs(node, 1)
+                # completely_biparted = bfs(node, 1)
+
+                # Return false if any part of the graph
+                # was not able to be biparted
+                if not completely_biparted:
+                    return False
         
+        # Entire graph was parted into 2 completely
+        return True
         
 # @lc code=end
 
