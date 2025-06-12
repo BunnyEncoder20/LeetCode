8621f46c0bf5a3cd5df35bfaa5b95aae84c13047
Varun Verma <varun.verma2024@gmail.com>
2025-06-12 13:25:07 +0530

    completed eventual safe nodes, BFS method

--- 802.find-eventual-safe-states.py
+++ 802.find-eventual-safe-states.py
@@ -5,8 +5,9 @@
 #
 
 # @lc code=start
+from collections import deque
 class Solution:
-    def eventualSafeNodes(self, adj: List[List[int]]) -> List[int]:
+    def eventualSafeNodes_dfs(self, adj: List[List[int]]) -> List[int]:
         def dfs(node):
             # mark
             vis[node] = 1
@@ -26,8 +27,8 @@ class Solution:
                         return True
             
             # No cycle found, so mark as safe node
-            isSafe[node] = 1
             pathvis[node] = 0
+            isSafe[node] = 1
             return False
 
 
@@ -46,6 +47,44 @@ class Solution:
         ans = [node for node in range(V) if isSafe[node]]
 
         return ans
+
+    def eventualSafeNodes(self, adj: List[List[int]]) -> List[int]:
         
+        def topologicalSort():
+            # make indegree list 
+            indeg = [0]*V 
+            for n in range(V):
+                for it in revAdj[n]:
+                    indeg[it] += 1
+            
+            # enque nodes with indeg = 0
+            q = deque(n for n in range(V) if indeg[n]==0)
+
+            # toposort
+            toposort = []
+            while q:
+                node = q.popleft()
+                toposort.append(node)
+                for it in revAdj[node]:
+                    indeg[it] -= 1
+                    if indeg[it] == 0:
+                        q.append(it)
+            
+            return toposort
+
+
+        # reverse graph; Make revAdj list
+        V = len(adj)
+        revAdj = [[] for _ in range(V)]
+        for i in range(V):
+            for it in adj[i]:
+                revAdj[it].append(i)
+        
+        # Toposort
+        safeNodes = topologicalSort()
+        safeNodes.sort()
+        return safeNodes
+
+
 # @lc code=end
 
