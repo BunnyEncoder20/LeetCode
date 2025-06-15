5aecc232eeae48ff955d3527654b0dc0bff93b4d
Varun Verma <varun.verma2024@gmail.com>
2025-06-15 20:58:43 +0530

    completed shortes path in binary mat

---
+++ 1091.shortest-path-in-binary-matrix.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=1091 lang=python3
+#
+# [1091] Shortest Path in Binary Matrix
+#
+
+# @lc code=start
+from collections import deque
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n,m = len(grid), len(grid[0])
+
+        # trivial case: src or destination node blocked
+        if grid[0][0] == 1 or grid[n-1][m-1] == 1: return -1
+
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        dist = [[float('inf')]*m for _ in range(n)]
+        q = deque()
+        
+        def isValid(i, j):
+            '''func to check if cell valid path cell'''
+            return (
+                0<=i<n and 
+                0<=j<m and 
+                grid[i][j] == 0
+            )
+        
+        # enque src ndoe
+        dist[0][0] = 1
+        q.append((1,0,0))
+
+        # BFS grid
+        while q:
+            distance, i, j = q.popleft()
+            
+            # check if reached destination
+            if i == n-1 and j == m-1:
+                return distance
+
+            for di, dj in directions:
+                ni, nj = i+di, j+dj 
+                if isValid(ni, nj) and distance + 1 < dist[ni][nj]:
+                    dist[ni][nj] = distance + 1
+                    q.append((distance+1, ni, nj))
+        
+        # path not found
+        return -1
+
+
+        
+# @lc code=end
+
