39be4352a79aadfe2f1f5c3afa20132be05b2513
Varun Verma <varun.verma2024@gmail.com>
2025-06-16 11:35:53 +0530

    completed path with minimum effort

--- 1631.path-with-minimum-effort.py
+++ 1631.path-with-minimum-effort.py
@@ -9,7 +9,37 @@ from typing import List
 import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        
-        
+        n, m = len(heights), len(heights[0])
+        destination = (n-1, m-1)
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        maxDiff_mat = [[float('inf')]*m for _ in range(n)] 
+        pq = []
+
+        def isValid(i, j):
+            return (0<=i<n and 0<=j<m)
+
+
+        maxDiff_mat[0][0] = 0        
+        heapq.heappush(pq, (0,0,0))  # (diff, i, j)
+
+        while pq:
+            diff, i, j = heapq.heappop(pq)
+
+            # return if destination
+            if (i, j) == destination:
+                return diff
+            
+            for di, dj in directions:
+                ni, nj = i+di, j+dj
+
+                if isValid(ni, nj):
+                    currDiff = abs(heights[ni][nj] - heights[i][j])
+                    newEffort = max(diff, currDiff)
+
+                    if newEffort < maxDiff_mat[ni][nj]:
+                        maxDiff_mat[ni][nj] = newEffort
+                        heapq.heappush(pq, (newEffort, ni, nj))
+
+        return -1
 # @lc code=end
 
