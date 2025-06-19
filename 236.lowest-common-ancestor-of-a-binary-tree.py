bcb946f0b1fce5e79ef3bab3603c21e29faa1183
Varun Verma <varun.verma2024@gmail.com>
2025-06-19 15:16:51 +0530

    breute LCA

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -14,17 +14,52 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # base case 
-        if root == None or root == p or root == q:
-            return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        """
+        Time Complexity: O(N), where N is the number of nodes in the tree
+        Space Complexity: O(H), where H is the height of the tree (for the recursion stack and path storage).
+        """
+        def findPath2Node(node, path, targetNode):
+            # base case
+            if not node:return
 
-        if left is None: return right
-        elif right is None: return left
-        else:
-            # this is our LCA
-            return root
+            # add node to path
+            path.append(node)
+
+            # if target node found
+            if node == targetNode:
+                return True
+            
+            # recursively go and search each ST
+            # If target found in any one of them,
+            # return True
+            if (
+                findPath2Node(node.left, path, targetNode) or
+                findPath2Node(node.right, path, targetNode)
+            ):
+                return True
+            
+            # else target node on this path, remove node 
+            # from the path and return false
+            path.pop()
+            return False
+
+
+        # Get path to each node 
+        p_path, q_path = [], []
+        findPath2Node(root, p_path, p)
+        findPath2Node(root, q_path, q)
+
+
+        # Till nodes are equal, keep going
+        ans = None
+        for pNode, qNode in zip(p_path, q_path):
+            if pNode == qNode:
+                ans = pNode
+            else:
+                break
+        
+        return ans
+
+        
 # @lc code=end
 
