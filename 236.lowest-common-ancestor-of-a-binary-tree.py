0db099072e9a53ec5b714b3d642fc658a8908a71
Varun Verma <varun.verma2024@gmail.com>
2025-06-19 18:02:57 +0530

    rev LCA in BT

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -14,51 +14,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        """
-        Time Complexity: O(N), where N is the number of nodes in the tree
-        Space Complexity: O(H), where H is the height of the tree (for the recursion stack and path storage).
-        """
-        def findPath2Node(node, path, targetNode):
-            # base case
-            if not node:return
+        # base case
+        if root is None or root == p or root == q:
+            return root
 
-            # add node to path
-            path.append(node)
+        LST_search = self.lowestCommonAncestor(root.left, p, q)
+        RST_search = self.lowestCommonAncestor(root.right, p, q)
 
-            # if target node found
-            if node == targetNode:
-                return True
-            
-            # recursively go and search each ST
-            # If target found in any one of them,
-            # return True
-            if (
-                findPath2Node(node.left, path, targetNode) or
-                findPath2Node(node.right, path, targetNode)
-            ):
-                return True
-            
-            # else target node on this path, remove node 
-            # from the path and return false
-            path.pop()
-            return False
-
-
-        # Get path to each node 
-        p_path, q_path = [], []
-        findPath2Node(root, p_path, p)
-        findPath2Node(root, q_path, q)
-
-
-        # Till nodes are equal, keep going
-        ans = None
-        for pNode, qNode in zip(p_path, q_path):
-            if pNode == qNode:
-                ans = pNode
-            else:
-                break
-        
-        return ans
+        if LST_search is None:
+            return RST_search
+        elif RST_search is None:
+            return LST_search
+        else:
+            return root
 
         
 # @lc code=end
