0944c83c2d1f55d4758681c702f1f5341ca0519e
Varun Verma <varun.verma2024@gmail.com>
2025-06-19 11:28:16 +0530

    rev peak elements

--- 162.find-peak-element.py
+++ 162.find-peak-element.py
@@ -13,18 +13,20 @@ class Solution:
         if n == 1: return 0
         if nums[0] > nums[1]: return 0
         if nums[n-2] < nums[n-1]: return n-1
-            
-        low,high = 1, n-2
         
+        
+        low,high = 1, n-2
         while low<=high:
             mid = (low+high)//2
-            if nums[mid-1] <= nums[mid] >= nums[mid+1]:
+
+            if nums[mid-1] < nums[mid] > nums[mid+1]:
                 return mid
-            elif nums[mid] < nums[mid+1]:
-                low = mid+1
+            elif nums[mid-1] < nums[mid]:
+                low = mid + 1
             else:
-                high = mid-1
+                high = mid - 1
         
         return -1
+        
 # @lc code=end
 
