cd2029ac01211fcae22decdf579ac73ddc687e46
Varun Verma <varun.verma2024@gmail.com>
2025-06-24 19:40:05 +0530

    completed RPN

--- 150.evaluate-reverse-polish-notation.py
+++ 150.evaluate-reverse-polish-notation.py
@@ -9,28 +9,39 @@ from typing import List
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        
+        operators = ('+','-','*','/')
+
         for ch in tokens:
-            if ch == '+':
-                n2, n1 = stack.pop(), stack.pop()
-                stack.append(n1+n2)
-            elif ch == '-':
-                n2, n1 = stack.pop(), stack.pop()
-                stack.append(n1-n2)
-            elif ch == '*':
-                n2, n1 = stack.pop(), stack.pop()
-                stack.append(n1*n2)
-            elif ch == '/':
-                n2, n1 = stack.pop(), stack.pop()
 
-                # use int() to make use to round
-                # the result towards 0
-                stack.append(int(n1/n2))    
+            # if it's a operator
+            if ch in operators:
+                # check if there are 2 nums to pop in stk
+                if len(stack)<2:
+                    raise ValueError(f"Not enough operands before operator '{ch}'")
 
-            # case: it's a number
+                n2, n1 = stack.pop(), stack.pop()
+                if ch=="+":
+                    stack.append(n1+n2)
+                elif ch=="-":
+                    stack.append(n1-n2)
+                elif ch=="*":
+                    stack.append(n1*n2)
+                elif ch=="/":
+                    # remember int for rounding towards 0
+                    stack.append(int(n1/n2))
+            
+            # if it's a number
             else:
-                stack.append(int(ch))
+                # try to insert,
+                try:
+                    stack.append(int(ch))
+                
+                # unless not int
+                except ValueError:
+                    raise ValueError(f"Invalid token: '{ch}'")
+
         
+        # check if stack
         return stack[0]
                 
         
