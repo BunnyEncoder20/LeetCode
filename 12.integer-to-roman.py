a09480eb16a78ff0c63ba37b2c48146cb505c643
Varun Verma <varun.verma2024@gmail.com>
2025-06-26 12:03:58 +0530

    complete integer to roamn

---
+++ 12.integer-to-roman.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=12 lang=python3
+#
+# [12] Integer to Roman
+#
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # mapping (will be going from MSB to LSB)
+        val_roman_mapping = [
+            (1000, 'M'),(900, 'CM'),(500, 'D'), 
+            (400, 'CD'),(100, 'C'), (90, 'XC'), 
+            (50, 'L'),  (40, 'XL'), (10, 'X'),
+            (9, 'IX'),  (5, 'V'),   (4, 'IV'), 
+            (1, 'I')
+        ]
+
+        resultant = ""
+        for value, symbol in val_roman_mapping:
+            # 2000 // 1000 = multiple x RN
+            # = 2 * 'M = 'MM'
+            # if multiple = 0, no symbol will be added
+            multiple = num // value
+            resultant += (symbol * multiple)
+
+            # update number
+            num %= value
+        
+        return resultant
+
+
+# @lc code=end
+
