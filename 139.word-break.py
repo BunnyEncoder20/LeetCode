6b42c4326b2558024893e48e7355c951f049c09d
Varun Verma <varun.verma2024@gmail.com>
2025-07-03 07:29:14 +0530

    dp word break table solution done

--- 139.word-break.py
+++ 139.word-break.py
@@ -8,19 +8,31 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        dp = [False] * n
+        dp = [False] * (n+1)
         
-        for i in range(n):
+        # if we at end of s, it's true
+        dp[n] = True
+
+        # iterate from back of s
+        for i in range(n,-1,-1):
+            # check if word matching s[i:word]
+
             for word in wordDict:
-                # i not of length of wordDict word
-                if i < len(word)-1: continue
-                
-                if i == len(word)-1 or dp[i-len(word)]:
-                    if s[i - len(word)+1 : i+1] == word:
-                        dp[i] = True
-                        break
-            
-        return dp[n-1]
+                # check s[i:word] has enough chars for word
+                if (
+                    i+len(word) <= len(s) and 
+                    s[i:i+len(word)] == word
+                ):
+                    dp[i] = dp[i + len(word)]
+
+                # if we found a word break, 
+                # we can move on to next idx,
+                if dp[i]:
+                    break
+        
+        # if we were able to word break entire s,
+        # dp[0] would be True
+        return dp[0]
         
 # @lc code=end
 
