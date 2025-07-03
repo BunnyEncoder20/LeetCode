923a417de2a77c8fa445319055ad666ceb06c579
Varun Verma <varun.verma2024@gmail.com>
2025-07-03 13:23:34 +0530

    rev coin change - II

--- 518.coin-change-ii.py
+++ 518.coin-change-ii.py
@@ -5,27 +5,37 @@
 #
 
 # @lc code=start
+from functools import cache, lru_cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # init 
+        @cache
+        def recursion(i, target):
+            # base case 
+            if i == 0:
+                if target % coins[0] == 0:
+                    return 1
+                else:
+                    return 0
+            
+            # init
+            pick, not_pick = 0, 0
+
+            # case we do not pick coin
+            not_pick = recursion(i-1, target)
+
+            # case we pick coin
+            if coins[i] <= target:
+                pick = recursion(i, target-coins[i])
+            
+            # return sum of total ways
+            return pick + not_pick
+
         n = len(coins)
-        prev = [0] * (amount+1)
-        curr = [0] * (amount+1)
-        
-        # init 0th row of dp table
-        for T in range(amount+1):
-            if T % coins[0] == 0: prev[T] = 1
-        
-        for i in range(1, n):
-            for T in range(amount+1):
-                pick, notpick = 0, 0
-                notpick = prev[T]
-                if coins[i] <= T:
-                    pick = curr[T - coins[i]]
-                curr[T] = pick + notpick
-            prev[:] = curr
-        
-        return prev[amount]
+        res = recursion(n-1, amount)
+        if res != int(1e9):
+            return res
+        else:
+            return 0
     
             
         
