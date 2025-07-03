66968b5e96bd08d2b75fd404aebfe7b049b4ffec
Varun Verma <varun.verma2024@gmail.com>
2025-07-03 18:49:05 +0530

    done rotate mat by 90 deg

---
+++ 48.rotate-image.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=48 lang=python3
+#
+# [48] Rotate Image
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # Step1: Transpose Matrix
+        self.transpose(matrix)
+
+        # Step2: reverse all rows in mat
+        for row in matrix:
+            row.reverse()
+        
+        # Step3: matrix rotated
+    
+    def transpose(self, mat):
+        rows, cols = len(mat), len(mat[0])
+        for i in range(rows):
+            for j in range(i+1, cols):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+        
+        
+        
+# @lc code=end
+
