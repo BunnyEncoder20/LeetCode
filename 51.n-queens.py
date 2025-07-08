31676e4405af27985094549c5851619829872468
Varun Verma <varun.verma2024@gmail.com>
2025-07-08 10:16:35 +0530

    rev N-queens

--- 51.n-queens.py
+++ 51.n-queens.py
@@ -6,58 +6,53 @@
 
 # @lc code=start
 class Solution:
-    def isSafe(self, board, row, col):
-      # check left upper diagonal
-      r,c = row,col
-      while r >= 0 and c >= 0:
-        if board[r][c] == "Q": return False
-        r -= 1
-        c -= 1
-        
-      # check left side
-      r,c = row,col
-      while c >= 0:
-        if board[r][c] == "Q": return False
-        c -= 1
-        
-      # check left lower diagonal
-      r,c = row,col
-      while r < len(board) and c >= 0:
-        if board[r][c] == "Q": return False
-        r += 1
-        c -= 1
-        
-      # no Queens in attacking pos
-      return True
-                
-        
-    def placeQueens(self, col, ans, board):
-      # base case 
-      if col == len(board):
-          # add board to ans
-          ans.append(board[:])
-          return
-      
-      # normally
-      for row in range(len(board)):
-        if self.isSafe(board, row, col):
-          # if the position is safe, place Q
-          board[row] = board[row][:col]+"Q"+board[row][col+1:]
-
-          # recursively go to next col and try to place another Q
-          self.placeQueens(col+1,ans,board)
-          
-          # remove Q for backtracking
-          board[row] = board[row][:col]+"."+board[row][col+1:]
-                
-
-        
     def solveNQueens(self, n: int) -> List[List[str]]:
-      board = ["."*n for _ in range(n)] # "....." for _ i range(n)
-      ans = []
-      self.placeQueens(0, ans, board)
-      return ans
+        board = ["."*n for _ in range(n)]
+        ans = []
+        self.placeNQueens(0, board, ans)
+        return ans
+    
+    def placeNQueens(self, col, board, ans):
+        n = len(board)
+
+        # base case
+        if col == n:
+            ans.append([row for row in board])
+            return
         
+        # try to place Q in each row for this col
+        for row in range(n):
+            if self.canPlace(board, row, col):
+                board[row] = board[row][:col] + "Q" + board[row][col+1:]
+                self.placeNQueens(col+1, board, ans)
+                board[row] = board[row][:col] + "." + board[row][col+1:]
+        
+        return
+    
+    def canPlace(self, board, row, col):
+        n = len(board)
+
+        # check Upper diagonal dir
+        i, j = row, col
+        while i>=0 and j>=0:
+            if board[i][j] == 'Q': return False
+            i -= 1
+            j -= 1
+        
+        # check left side of row
+        j = col
+        while j>= 0:
+            if board[row][j] == 'Q': return False
+            j -= 1
+        
+        # check lower diagonal dir
+        i,j = row, col
+        while i<n and j>=0:
+            if board[i][j] == 'Q': return False
+            i += 1
+            j -= 1
+
+        return True        
         
 # @lc code=end
 
