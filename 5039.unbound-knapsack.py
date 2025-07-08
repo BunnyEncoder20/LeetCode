47270c7ffa8b33d3b5586169de501dabc95e115c
Varun Verma <varun.verma2024@gmail.com>
2025-07-08 13:03:58 +0530

    completed unbound knapsac problem memo

--- 5039.unbound-knapsack.py
+++ 5039.unbound-knapsack.py
@@ -7,7 +7,7 @@ class Solution1:
                 if weights[i] <= current_capacity:
                     return (current_capacity // weights[0]) * (values[0])
                 else:
-                    return 0 
+                    return 0
 
             notpick = 0 + recursion(i-1, current_capacity)
             pick = int(-1e9)
@@ -30,13 +30,20 @@ class Solution2:
                 else:
                     return 0
 
+            # check in dp
+            if dp[i][current_capacity] != -1:
+                return dp[i][current_capacity]
+
+            # calc pick and not pick
             notpick = 0 + recursion(i-1, current_capacity)
             pick = int(-1e9)
             if weights[i] <= current_capacity:
                 # to pick same item mulitple times, don't move i
                 pick = values[i] + recursion(i, current_capacity-weights[i])
 
-            return max(pick, notpick)
+            # update the dp memo
+            dp[i][current_capacity] = max(pick, notpick)
+            return dp[i][current_capacity]
 
         dp = [[-1]*(maxW+1) for _ in range(n)]
         return recursion(n-1, maxW)
