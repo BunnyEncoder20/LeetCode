4c76940056fc96a148d74c33e78f7c624bb3ba6a
Varun Verma <varun.verma2024@gmail.com>
2025-07-11 14:10:53 +0530

    done better and optimal apporahc of set matrix zero

--- 5042.set-matrix-zero.py
+++ 5042.set-matrix-zero.py
@@ -31,3 +31,61 @@ class Solution1:
                     matrix[i][j] = 0
 
         return
+
+class Solution2:
+    '''
+        TC: 2 x O(NxM)
+        SC: O(n) + O(m)
+    '''
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix),len(matrix[0])
+        rowMarked = [0]*rows
+        colMarked = [0]*cols
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    rowMarked[i] = 1
+                    colMarked[j] = 1
+
+        for i in range(rows):
+            for j in range(cols):
+                if rowMarked[i] or colMarked[j]:
+                    matrix[i][j] = 0
+
+        return
+
+
+class Solution3:
+    '''
+        TC: 2 x O(N+M) + O(NxM)
+        SC: O(1)
+    '''
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix),len(matrix[0])
+        first_row_zero = any(matrix[0][j]==0 for j in range(cols))
+        first_col_zero = any(matrix[i][0]==0 for i in range(rows))
+
+        # Using first row and col as lists, mark zero locations
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+        # Set zeros based on markings
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j] = 0
+
+        # handling the first row and col
+        if first_row_zero:
+            for j in range(cols):
+                matrix[0][j] = 0
+
+        if first_col_zero:
+            for i in range(rows):
+                matrix[i][0] = 0
+
+        return
