62e5e65537f51a50bf120698bd385e676d2c2e7e
Varun Verma <varun.verma2024@gmail.com>
2025-07-12 17:29:36 +0530

    rev trapped rainwater

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -6,13 +6,43 @@
 
 # @lc code=start
 from typing import List
-class Solution:
+
+class Solution1:
     def trap(self, height: List[int]) -> int:
-        # 2 pointers approach
+        n = len(height)
+        leftMax = self.getPrefixMaximum(height, n)
+        rightMax = self.getSuffixMaximum(height, n)
+
+        trappedWater = 0
+        for i in range(1,n-1):
+            water = min(leftMax[i], rightMax[i]) - height[i]
+            if water > 0: trappedWater += water
+
+        return trappedWater
+
+    def getPrefixMaximum(self, height, n) -> List:
+        prefixMaximum = [0]*n
+        prefixMaximum[0] = height[0]
+        for i in range(1, n):
+            prefixMaximum[i] = max(prefixMaximum[i-1], height[i])
+        return prefixMaximum
+
+    def getSuffixMaximum(self, height, n) -> List:
+        suffixMaximum = [0]*n
+        suffixMaximum[-1] = height[-1]
+        for i in range(n-2, -1, -1):
+            suffixMaximum[i] = max(suffixMaximum[i+1], height[i])
+        return suffixMaximum
+
+
+class Solution2:
+    def trap(self, height: List[int]) -> int:
+        # 2 pointers
         left, right = 0, len(height)-1
         leftMax, rightMax = 0, 0
         trapped_rainwater = 0
 
+        # always move the smaller ptr
         while left<right:
             if height[left] <= height[right]:
                 if height[left] < leftMax:
@@ -26,7 +56,7 @@ class Solution:
                 else:
                     rightMax = height[right]
                 right -= 1
-        
-        return trapped_rainwater
-# @lc code=end
 
+        return trapped_rainwater
+
+# @lc code=end
