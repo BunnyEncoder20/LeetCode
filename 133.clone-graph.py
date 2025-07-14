46d2b065e64e2b2affabab487288d26d9b4ee6c0
Varun Verma <varun.verma2024@gmail.com>
2025-07-14 18:25:57 +0530

    revisied clone graph

--- 133.clone-graph.py
+++ 133.clone-graph.py
@@ -12,39 +12,31 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
 
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # trivial case
-        if not node: return 
-        
-        # helper func
-        def clone_dfs(node):
-            '''
-            returns the copy node of the node sent.
-            '''
-
-            # if node visited
+        def cloneNode(node):
+            # base case
             if node in mpp:
                 return mpp[node]
 
-            # make a copy of curr node
+            # copy the node into hashmap
             copyNode = Node(node.val)
             mpp[node] = copyNode
 
-            # dfs for neighbouring nodes
-            # and append the copy of neighbours 
-            # into the copy node's neighbours
             for nn in node.neighbors:
-                copyNode.neighbors.append(clone_dfs(nn))
-            
-            return copyNode
-            
-            
-        # hashmap to map old nodes to new nodes
-        mpp = {}
-        return clone_dfs(node)
-        
-# @lc code=end
+                copyNode.neighbors.append(cloneNode(nn))
 
+            return copyNode
+
+        mpp = {}
+        return cloneNode(node)
+
+
+# @lc code=end
