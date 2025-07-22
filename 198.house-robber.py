a38a54c518f8bb84ee96c971baeecd0ac680b82d
Varun Verma <varun.verma2024@gmail.com>
2025-07-22 14:39:12 +0530

    rev house robber

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -5,31 +5,52 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         def recursive(i):
             # base case
             if i == 0: return nums[0]
             if i < 0: return 0
-            
-            # dp check
-            if dp[i] != -1: return dp[i]
-            
-            # normally
-            # not rob
-            notrob = 0 + recursive(i-1)
 
-            # rob
-            rob = nums[i] + recursive(i-2)
+            # check memo
+            if dp[i] != -1:
+                return dp[i]
 
-            # return the best
-            dp[i] = max(rob, notrob)
+            # pick
+            pick = nums[i] + recursive(i-2)
+
+            # not pick
+            notpick = 0 + recursive(i-1)
+
+            dp[i] = max(pick, notpick)
             return dp[i]
-        
+
         n = len(nums)
-        dp = [-1] * n
+        dp = [-1]*n
         return recursive(n-1)
-        
-        
+
+
 # @lc code=end
+
+# Test cases
+if __name__ == '__main__':
+    solver = Solution()
+    tests = [
+        ([1, 2, 3, 1], 4),
+        ([2, 7, 9, 3, 1], 12),
+        ([], 0),
+        ([10], 10),
+        ([2, 1], 2),
+        ([1, 2], 2),
+        ([0, 0, 0, 0], 0),
+        ([2, 1, 1, 2], 4),
+        ([5, 5, 5, 5, 5], 15),
+        ([10, 1, 10, 1, 10], 30)
+    ]
+
+    for i, (nums, expected) in enumerate(tests):
+        output = solver.rob(nums)
+        status = "Passed" if output == expected else "Failed"
+        print(f"testcase{i+1}: {nums} | {output} | {expected} | {status}")
 
